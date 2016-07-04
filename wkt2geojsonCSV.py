7c2b710190960ecd636a10c04b6918d5e1b94c68
MrSambo <kangwasambo@gmail.com>
2016-07-04 17:44:52 +0000

    Modified script to parse St Lucia data files.

--- wkt2geojsonCSV.py
+++ wkt2geojsonCSV.py
@@ -8,25 +8,26 @@ import geojson
 csv.field_size_limit(sys.maxsize)
 
 input_filename = sys.argv[1]
-output_filename = sys.argv[2]
+output_filename = "parsed_output/" + sys.argv[1]
 
 
 csvfile = open(input_filename, 'rb')
 reader = csv.DictReader(csvfile)
 
-# Rename WKT field (which is 1st field) to GeoJSON
-reader.fieldnames[0] = 'GeoJSON'
+# Rename the_geom field to GeoJSON
+# print "%s" % (reader.fieldnames[1]);
+reader.fieldnames[1] = 'GeoJSON'
 
 c = csv.DictWriter(open(output_filename, "wb"), fieldnames=reader.fieldnames)
 c.writeheader()
 
 for row in reader:
     for col in row:
-        if col == 'WKT':
-            d_wkt = row['WKT']
+        if col == 'GeoJSON':
+            d_wkt = row['GeoJSON']
             js1 = shapely.wkt.loads(d_wkt)
             js2 = geojson.Feature(geometry=js1, properties={})
-            row['WKT'] = js2.geometry
+            row['GeoJSON'] = js2.geometry
 
     c.writerow(row)
 
